831f583d4897302af43a5f37bb9be7338c297070
imakyo97 <kyoheishogun.1997@gmail.com>
2024-04-03 22:14:56 +0900

    Initial Commit

---
+++ EmbeddedFrameworkPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmbeddedFrameworkPractice
+//
+//  Created by 今村京平 on 2024/04/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
